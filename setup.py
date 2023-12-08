39e083d42f85821c9a4fa54eab9f6987770ece54
Maximilian Strauss <straussmaximilian@gmail.com>
2023-12-08 17:38:25 +0100

    0.1.4

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/straussmaximilian/ocrmac",
-    version="0.1.3",
+    version="0.1.4",
     zip_safe=False,
 )
