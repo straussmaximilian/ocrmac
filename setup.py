a38d01c653272b30a6bb2d6f8655956ab678cc38
Maximilian Strauss <straussmaximilian@gmail.com>
2026-01-08 17:41:16 +0100

    fix: update minimum Python version to 3.9
    - pyobjc requires Python 3.9+
    - Update classifiers to reflect supported versions (3.9-3.12)
    - Bump development status to Beta

--- setup.py
+++ setup.py
@@ -19,17 +19,18 @@ test_requirements = [
 setup(
     author="Maximilian Strauss",
     author_email="straussmaximilian@gmail.com",
-    python_requires=">=3.6",
+    python_requires=">=3.9",
     
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     description="A python wrapper to extract text from images on a mac system. Uses the vision framework from Apple.",
     entry_points={
